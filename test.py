dcab0658955012fe340db00330aa4e1eb61deca7
Vanshika110 <111649312+Vanshika110@users.noreply.github.com>
2024-04-06 00:31:58 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,118 @@
+# first=int(input("enter first:"))  //power
+# print(first**2)
+
+""" list1=[1,2,3] //pallindrome
+copy1=list1.copy()
+copy1.reverse()
+if(copy1==list1):
+    print("pallin")
+else:
+    print("not")
+
+list=["A","B", "C","A"]
+a=list.count("A")  
+print(a)
+b=list.sort()
+print(list)
+
+van={       #dictionary
+"name" : "vanshika",
+"sub":["coa","m4","python"],
+"num":{  #nested dict
+"phy":92,
+"chem":77
+}
+"year":2,
+"age":19,
+"isadult" :True
+}
+print(van)
+print(type(van))
+print(van["year"])
+van["name"]="leena"
+print(van["num"]["chem"])
+print(len(van)) 
+pairs=list(student.items())
+print(pairs[2])
+print(student.get("van"))
+student.update({city:prayagraj})
+print(student)
+#print(stu["name2"]) error
+print(stu.get("name2")) none
+print(stu.keys())
+print(list(stu.keys()))
+print(stu.values())
+print(list(stu.values()))
+print(stu.items())  #tuples
+
+null_dict={}
+null_dict["name"]="priya"
+print(null_dict) 
+
+set={2,5,3,"dvc","ghn",3,3}
+print(set)
+print(type(set))
+print(len(set))
+
+coll=set() #empty set
+coll.add(1)
+coll.add(2)
+coll.add(3)
+coll.pop()
+print(coll)
+coll.remove(2)
+coll.clear()
+print(coll)
+
+set1={3,6,2,1,6}
+set2={2,9,5,31,5}
+print(set1.union(set2))
+print(set1.intersection(set2)) 
+
+i=1
+while i<10:
+    print(3*i)
+    i+=1   
+
+list=[12,45,7,3,9]
+tuple=()
+for val in list:
+    print(val)
+
+for i in range(5): # stop*  
+    print(i)
+for i in range(5,9,2): #start stop* step 
+    print(i) 
+for el in range(2):
+    pass    #to skip"""
+    
+
+f=open("sample.py","w")
+f.write("hello")
+f.close()
+f=open("sample.py","at")
+f.write("\n i am vanshika ")
+f.close()
+f=open("sample.py","r+")
+f.write("\n i am vanshika ")
+f.close()
+f=open("sample.py","a+")
+f.write("\n i am priya ")
+f.close()
+f=open("sample.py","r")
+print(f.read())
+with open("sample.py","a") as f:  #automa
+    data=f.write("byee")
+    print(data)
+
+
+
+
+
+
+
+
+
+
+
+
